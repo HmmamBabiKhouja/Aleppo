37d46d6dd39481aa21dbd77967c13fcf952d197a
Hmmambk1994 <bhmmam@gmail.com>
2019-08-27 18:19:54 +0200

    the slider code works fine, befre adding the dots functions

--- code/picture-slider.js
+++ code/picture-slider.js
@@ -1,1 +1,25 @@
-let images = document.get
+let slideIndex=1;
+showDivs(slideIndex);
+
+
+
+
+function slide(num){
+    showDivs(slideIndex+=num);
+}
+
+function showDivs(num){
+    let images = document.getElementsByClassName("images");
+    if(slideIndex>images.length){
+        slideIndex=1;
+    }
+    if(slideIndex<1){
+        slideIndex=images.length;
+    }
+
+    for(let i=0;i<images.length;i++){
+        images[i].style.display="none";
+    }
+
+    images[slideIndex-1].style.display="block";
+}
