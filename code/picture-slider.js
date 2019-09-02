ec434bf2455d2212edbea56cb56fc0aeb48c49ce
Hmmambk1994 <bhmmam@gmail.com>
2019-09-02 05:44:15 +0200

    trying to have two slideshows

--- code/picture-slider.js
+++ code/picture-slider.js
@@ -1,33 +1,44 @@
-let slideIndex=1;
-showDivs(slideIndex);
+let castleIndex = 1;
+let greatMosqueIndex = 1;
 
-function dotJump(num){
-    showDivs(slideIndex=num);
+showDivs(castleIndex,"castle");
+showDivs(greatMosqueIndex,"great-mosque");
+
+function dotJump(num, className) {
+    if (className === "castle") {
+        showDivs((castleIndex = num),"castle","castle-dot");
+    } else if (className === "great-mosque") {
+        showDivs((greatMosqueIndex = num),"great-mosque","great-mosque-dot");
+    }
 }
 
-function slide(num){
-    showDivs(slideIndex+=num);
+function slide(num, className) {
+    if (className === "castle") {
+        showDivs((castleIndex += num),"castle","castle-dot");
+    } else if (className === "great-mosque") {
+        showDivs((greatMosqueIndex += num),"great-mosque","great-mosque-dot");
+    }  
 }
 
-function showDivs(num){
-    let images = document.getElementsByClassName("image");
-    let dots = document.getElementsByClassName("dot");
+function showDivs(num,className,dotName) {
+  let images = document.getElementsByClassName(className);
+  let dots = document.getElementsByClassName(dotName);
 
-    if(slideIndex>images.length){
-        slideIndex=1;
-    }
-    if(slideIndex<1){
-        slideIndex=images.length;
-    }
+  if (slideIndex > images.length) {
+    slideIndex = 1;
+  }
+  if (slideIndex < 1) {
+    slideIndex = images.length;
+  }
 
-    for(let i=0;i<images.length;i++){
-        images[i].style.display="none";
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
 
-    // for(let i=0;i<dots.length;i++){
-    // }
-    
-    dots[slideIndex - 1].className += " active";
-    images[slideIndex-1].style.display="block";
+  // for(let i=0;i<dots.length;i++){
+  // }
+
+  dots[slideIndex - 1].className += " active";
+  images[slideIndex - 1].style.display = "block";
 }
