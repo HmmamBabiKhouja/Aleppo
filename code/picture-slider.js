279a7ad7b04d0337744b2367e57692b92992e914
Hmmambk1994 <bhmmam@gmail.com>
2019-09-18 06:33:17 +0200

    img title is added

--- code/picture-slider.js
+++ code/picture-slider.js
@@ -11,23 +11,27 @@ function slide(num){
 
 function showDivs(num){
     let images = document.getElementsByClassName("img-slider");
+    let title= document.getElementsByClassName("img-title");
     let dots = document.getElementsByClassName("dot");
 
     if(slideIndex>images.length){
         slideIndex=1;
     }
+
     if(slideIndex<1){
         slideIndex=images.length;
     }
 
     for(let i=0;i<images.length;i++){
         images[i].style.display="none";
+        title[i].style.display="none";
         dots[i].className = dots[i].className.replace(" active", "");
     }
 
     // for(let i=0;i<dots.length;i++){
     // }
     
-    dots[slideIndex - 1].className += " active";
     images[slideIndex-1].style.display="block";
+    title[slideIndex-1].style.display="block";
+    dots[slideIndex - 1].className += " active";
 }
