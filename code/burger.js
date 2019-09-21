6b194e82f04b15e59b8a7bfdc4b943f1e1d3b844
Hmmambk1994 <bhmmam@gmail.com>
2019-09-21 14:34:11 +0200

    click page-content to draw-back the taps list

--- code/burger.js
+++ code/burger.js
@@ -1,16 +1,15 @@
 let burger = document.querySelector(".burger");
 let burgerLine = document.querySelectorAll(".burger-line");
-let tabsList = document.querySelector(".tabs-container");
+let tabsContainer = document.querySelector(".tabs-container");
 
 burger.addEventListener("click", () => {
-    tabsList.classList.toggle("tabs-container-active");
+    tabsContainer.classList.toggle("tabs-container-active");
     burger.classList.toggle("burger-active");
     burgerLineColorChange();
 });
 
-function burgerLineColorChange(){
-    for (let i=0;i<burgerLine.length;i++){
-        burgerLine[i].classList.toggle("burger-line-active");
-    }
+function burgerLineColorChange() {
+  for (let i = 0; i < burgerLine.length; i++) {
+    burgerLine[i].classList.toggle("burger-line-active");
+  }
 }
-
