a4cd2280d4ed2626dc05f38b8d7309d6d0aa02cc
Hmmambk1994 <bhmmam@gmail.com>
2019-10-05 21:23:50 +0200

    they added

--- code/picture-slider.js
+++ code/picture-slider.js
@@ -10,29 +10,24 @@ function slide(num){
 }
 
 function showDivs(num){
-    
     let images = document.getElementsByClassName("img-slider");
-    let title= document.getElementsByClassName("img-title");
     let dots = document.getElementsByClassName("dot");
 
     if(slideIndex>images.length){
         slideIndex=1;
     }
-
     if(slideIndex<1){
         slideIndex=images.length;
     }
 
     for(let i=0;i<images.length;i++){
         images[i].style.display="none";
-        title[i].style.display="none";
-        dots[i].className = dots[i].className.replace(" dot-active", "");
+        dots[i].className = dots[i].className.replace(" active", "");
     }
 
     // for(let i=0;i<dots.length;i++){
     // }
-    alert(images.length);
+    
+    dots[slideIndex - 1].className += " active";
     images[slideIndex-1].style.display="block";
-    title[slideIndex-1].style.display="block";
-    dots[slideIndex - 1].className += " dot-active";
 }
