31bf1bd05b20aaf36a7b3f5470b79a7f3cd5e4f0
Hmmambk1994 <bhmmam@gmail.com>
2019-10-06 06:29:38 +0200

    stable version of mosque and castle, working on adding the insudstry and food

--- code/picture-slider.js
+++ code/picture-slider.js
@@ -1,25 +1,40 @@
-let slideIndex=1;
-showDivs(slideIndex);
+let castle = { slideIndex: 1, name: "castle-" };
+let mosque = { slideIndex: 1, name: "mosque-" };
+let arr=[mosque,castle];
 
-function dotJump(num){
-    showDivs(slideIndex=num);
-}
+showDivs(castle);
+showDivs(mosque);
 
-function slide(num){
-    showDivs(slideIndex+=num);
-}
-
-function showDivs(num){
-    let images = document.getElementsByClassName("img-slider");
-    let titles = document.getElementsByClassName("img-title");
-    let dots = document.getElementsByClassName("dot");
-
-
-    if(slideIndex>images.length){
-        slideIndex=1;
+function dotJump(num,className){
+    for(let i = 0 ;i<arr.length;i++){
+        if(arr[i].name===className){
+            console.log(arr[i]);
+            arr[i].slideIndex=num;
+            showDivs(arr[i]);
+        }
     }
-    if(slideIndex<1){
-        slideIndex=images.length;
+}
+
+function slide(num,className){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].name === className) {
+            arr[i].slideIndex += num;
+            showDivs(arr[i]);
+        }
+    }
+}
+
+function showDivs(obj){
+    let images = document.getElementsByClassName(obj.name+"img-slider");
+    let titles = document.getElementsByClassName(obj.name+"img-title");
+    let dots = document.getElementsByClassName(obj.name+"dot");
+
+
+    if(obj.slideIndex>images.length){
+        obj.slideIndex=1;
+    }
+    if(obj.slideIndex<1){
+        obj.slideIndex=images.length;
     }
 
     for(let i=0;i<images.length;i++){
@@ -28,7 +43,7 @@ function showDivs(num){
         dots[i].className = dots[i].className.replace(" dot-active", "");
     }
 
-    titles[slideIndex - 1].style.display="block";
-    images[slideIndex - 1].style.display="block";
-    dots[slideIndex - 1].className += " dot-active";
+    titles[obj.slideIndex - 1].style.display="block";
+    images[obj.slideIndex - 1].style.display="block";
+    dots[obj.slideIndex - 1].className += " dot-active";
 }
