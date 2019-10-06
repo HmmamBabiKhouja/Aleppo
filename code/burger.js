98aaa0db5e9630590d0500dbdb9a6f9521e048b8
Hmmambk1994 <bhmmam@gmail.com>
2019-10-06 17:40:50 +0200

    stable version of burger list

---
+++ code/burger.js
@@ -0,0 +1,15 @@
+let burger = document.querySelector(".burger");
+let burgerLine = document.querySelectorAll(".burger-line");
+let tabsContainer = document.querySelector(".tabs-container");
+
+burger.addEventListener("click", () => {
+    tabsContainer.classList.toggle("tabs-container-active");
+    burger.classList.toggle("burger-active");
+    burgerLineColorChange();
+});
+
+function burgerLineColorChange() {
+    for (let i = 0; i < burgerLine.length; i++) {
+        burgerLine[i].classList.toggle("burger-line-active");
+    }
+}
